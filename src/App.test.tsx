d18483567f2b2ac223eb5193734f099a70c945f0
Barsegyan <barsegyan@topsystems.ru>
2025-05-29 13:43:41 +0300

    fix merg conflict

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { AppWithLogger } from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<AppWithLogger />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
