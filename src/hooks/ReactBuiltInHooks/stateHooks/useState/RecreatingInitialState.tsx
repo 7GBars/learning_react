19c8d605a93ac6706356e3716b1a067df43c37f6
Barsegyan <barsegyan@topsystems.ru>
2025-10-14 18:27:41 +0300

    hooks / useState: -пример RecreatingInitialState  реинициализация самих данных не происходит

--- src/hooks/ReactBuiltInHooks/stateHooks/useState/RecreatingInitialState.tsx
+++ src/hooks/ReactBuiltInHooks/stateHooks/useState/RecreatingInitialState.tsx
@@ -6,11 +6,12 @@ interface Todo {
 }
 
 function createInitialTodos(): Todo[] {
+  debugger
   const initialTodos: Todo[] = [];
   for (let i = 0; i < 50; i++) {
     initialTodos.push({
       id: i,
-      text: 'Item ' + (i + 1)
+      text: 'Item ' + (i + Math.random() * 10)
     });
   }
   return initialTodos;
@@ -19,7 +20,35 @@ function createInitialTodos(): Todo[] {
 interface TUseStateSimpleProps {}
 
 export const RecreatingInitialState: React.FC<TUseStateSimpleProps> = ({}) => {
-  const [todos, setTodos] = React.useState<Todo[]>(createInitialTodos());
+  const [todos, setTodos] = React.useState<Todo[]>(createInitialTodos()); // а тут createInitialTodos(), то есть результат её вызова.
+  const [text, setText] = React.useState<string>('');
+
+  return (
+    <>
+      <input
+        value={text}
+        onChange={e => setText(e.target.value)}
+      />
+      <button onClick={() => {
+        setText('');
+        setTodos([{
+          id: todos.length,
+          text: text
+        }, ...todos]);
+      }}>Add</button>
+      <ul>
+        {todos.map(item => (
+          <li key={item.id}>
+            {item.text}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export const WithoutRecreatingInitialState: React.FC<TUseStateSimpleProps> = ({}) => {
+  const [todos, setTodos] = React.useState<Todo[]>(createInitialTodos); // В этом примере передаётся функция инициализации, поэтому функция createInitialTodos выполняется только во время инициализации.
   const [text, setText] = React.useState<string>('');
 
   return (
