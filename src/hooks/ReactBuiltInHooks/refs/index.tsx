043114309ddb26cb8f7cf80238c3df04388213cb
geo bars <georgiybarsegian@yandex.com>
2025-11-13 21:09:44 +0300

    hooks / useRef: -тестирование на сравнение ссылок объектов пережанных в реф

--- src/hooks/ReactBuiltInHooks/refs/index.tsx
+++ src/hooks/ReactBuiltInHooks/refs/index.tsx
@@ -1,13 +1,30 @@
-import React, { useRef } from 'react';
+import React, {useEffect, useRef} from 'react';
 
 type TSimpleUseRefProps = {};
 
+const objectLiteralOutComponent = {name: 'ge'};
+
 export const SimpleUseRef: React.FC<TSimpleUseRefProps> = ({}) => {
+    const [count, setCount] = React.useState(0);
+
+    const objectLiteralInComponent = {name: 'ge'};
+
     const simpleRef = useRef(0);
-    const objectRef = useRef({name: 'ge'});
+    const objectRef = useRef(objectLiteralInComponent);
+    const objectRef2 = useRef(objectLiteralOutComponent);
     const domRef = useRef(null);
+
+
+    useEffect(() => {
+        console.log('simpleRef', simpleRef);
+        console.log('objectRef', objectRef.current === objectLiteralInComponent);
+        console.log('objectRef2', objectRef2.current === objectLiteralOutComponent);
+        console.log('domRef', domRef);
+    }, [count]);
     return (
         <>
+            <br/>
+            <input type={'number'} onChange={(e) => setCount(+e.target.value)} value={count}/>
            <div ref={domRef}>test</div>
     </>
 );
