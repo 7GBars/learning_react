1a7cecf739a0bb392dda853a0827defc5a87a304
Barsegyan <barsegyan@topsystems.ru>
2025-11-20 18:06:02 +0300

    fix code structure

--- src/hooks/ReactBuiltInHooks/refs/index.tsx
+++ src/hooks/ReactBuiltInHooks/refs/index.tsx
@@ -1,44 +1,44 @@
-import React, {useEffect, useRef} from 'react';
-import {UseImperativeHandle} from "@/hooks/ReactBuiltInHooks/refs/UseImperativeHandle";
-import {Button} from "primereact/button";
+import React, { useEffect, useRef } from 'react';
+import { UseImperativeHandle } from "@/hooks/ReactBuiltInHooks/refs/UseImperativeHandle";
+import { Button } from "primereact/button";
 
 type TSimpleUseRefProps = {};
 
-const objectLiteralOutComponent = {name: 'ge'};
+const objectLiteralOutComponent = { name: 'ge' };
 
 export const SimpleUseRef: React.FC<TSimpleUseRefProps> = ({}) => {
-    const [count, setCount] = React.useState(0);
+  const [count, setCount] = React.useState(0);
 
-    const objectLiteralInComponent = {name: 'ge'};
+  const objectLiteralInComponent = { name: 'ge' };
 
-    const simpleRef = useRef(0);
-    const objectRef = useRef(objectLiteralInComponent);
-    const objectRef2 = useRef(objectLiteralOutComponent);
-    const domRef = useRef(null);
-    const refForChildrenComponent = useRef(null);
+  const simpleRef = useRef(0);
+  const objectRef = useRef(objectLiteralInComponent);
+  const objectRef2 = useRef(objectLiteralOutComponent);
+  const domRef = useRef(null);
+  const refForChildrenComponent = useRef(null);
 
 
-    useEffect(() => {
+  useEffect(() => {
+    console.log('simpleRef', simpleRef);
+    console.log('objectRef', objectRef.current === objectLiteralInComponent);
+    console.log('objectRef2', objectRef2.current === objectLiteralOutComponent);
+    console.log('domRef', domRef);
+    console.log('refForChildrenComponent', refForChildrenComponent);
+  }, [count]);
+  return (
+    <>
+      <br/>
+      <input type={'number'} onChange={(e) => setCount(+e.target.value)} value={count}/>
+      <UseImperativeHandle ref={refForChildrenComponent}/>
+
+      <Button onClick={() => {
         console.log('simpleRef', simpleRef);
         console.log('objectRef', objectRef.current === objectLiteralInComponent);
         console.log('objectRef2', objectRef2.current === objectLiteralOutComponent);
         console.log('domRef', domRef);
         console.log('refForChildrenComponent', refForChildrenComponent);
-    }, [count]);
-    return (
-        <>
-            <br/>
-            <input type={'number'} onChange={(e) => setCount(+e.target.value)} value={count}/>
-            <UseImperativeHandle ref={refForChildrenComponent} />
-
-            <Button onClick={() => {
-                console.log('simpleRef', simpleRef);
-                console.log('objectRef', objectRef.current === objectLiteralInComponent);
-                console.log('objectRef2', objectRef2.current === objectLiteralOutComponent);
-                console.log('domRef', domRef);
-                console.log('refForChildrenComponent', refForChildrenComponent);
-            }}>get refs values</Button>
-           <div ref={domRef}>test</div>
+      }}>get refs values</Button>
+      <div ref={domRef}>test</div>
     </>
-);
+  );
 }
