41a8a720db1bed22a43af61ad6573d5c8ab629cb
geo bars <georgiybarsegian@yandex.com>
2025-12-11 21:04:11 +0300

    структурный рефактор

---
+++ src/hooks/ReactBuiltInHooks/effectHooks/webDevSimplified/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Example1() {
+    return (
+        <div></div>
+    );
+} /** https://www.youtube.com/watch?v=0ZJgIjIuY7U&list=PLZlA0Gpn_vH8EtggFGERCwMY5u5hOjf-h&index=2 **/
+
+
